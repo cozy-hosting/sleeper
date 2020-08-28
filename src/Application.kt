f546a47be221f79b13e79e9c43c6e6942eceb32e
Samuel Oechsler <samuel@oechsler.it>
2020-08-28 12:18:06 +0200

    Remove unused parts of the entrypoint

--- src/Application.kt
+++ src/Application.kt
@@ -2,12 +2,17 @@ package cozy
 
 import io.ktor.application.*
 import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>): Unit = EngineMain.main(args)
 
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+@Suppress("unused")
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respond("Hello World")
+        }
+    }
 }
 

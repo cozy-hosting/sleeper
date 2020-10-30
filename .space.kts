0d5730cb093fe763b1dba50d6a4416e90d85ed70
Samuel Oechsler <samuel@oechsler.it>
2020-10-30 14:28:33 +0000

    Update automation script triggers
    Only trigger on code reviews but not on push

--- .space.kts
+++ .space.kts
@@ -1,8 +1,30 @@
 val openjdkVersion = "15"
+val gradleVersion = "latest"
+
+var currentBranch: String? = null
+
+job("Initialize automation") {
+    container("openjdk:$openjdkVersion") {
+        kotlinScript { api ->
+            currentBranch = api.gitBranch()
+        }
+    }
+}
 
 job("Build & test project") {
-    parallel {
-        gradlew("openjdk:$openjdkVersion", "assemble")
-        gradlew("openjdk:$openjdkVersion", "test")
+    startOn {
+        gitPush {
+            val isCurrentBranchMaster = currentBranch == "master"
+            enabled = isCurrentBranchMaster
+        }
+
+        codeReviewOpened()
+        codeReviewClosed()
+    }
+
+    container("gradle:$gradleVersion") {
+        shellScript {
+            content = "gradle build"
+        }
     }
 }

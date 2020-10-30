522e028d0d469ed40147e0909c91167f01b6ab01
Samuel Oechsler <samuel@oechsler.it>
2020-10-30 14:37:23 +0000

    Update automation triggers yet again

--- .space.kts
+++ .space.kts
@@ -1,21 +1,10 @@
 val openjdkVersion = "15"
 val gradleVersion = "latest"
 
-var currentBranch: String? = null
-
-job("Initialize automation") {
-    container("openjdk:$openjdkVersion") {
-        kotlinScript { api ->
-            currentBranch = api.gitBranch()
-        }
-    }
-}
-
 job("Build & test project") {
     startOn {
         gitPush {
-            val isCurrentBranchMaster = currentBranch == "master"
-            enabled = isCurrentBranchMaster
+            enabled = false
         }
 
         codeReviewOpened()
